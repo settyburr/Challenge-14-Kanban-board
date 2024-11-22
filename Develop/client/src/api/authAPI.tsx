6bb56876cd0868fef309c8307ab3f5ca6659c7fa
Seth Burrow <sethburrow01@gmail.com>
2024-11-22 10:29:36 -0600

    copied over authapi and auth.ts from other folder i started in

--- Develop/client/src/api/authAPI.tsx
+++ Develop/client/src/api/authAPI.tsx
@@ -1,9 +1,26 @@
 import { UserLogin } from "../interfaces/UserLogin";
+import axios from 'axios';
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-}
+  try {
+    const response = await axios.post('/auth/login', userInfo, {
+      headers: {
+        'Content-Type': 'aplication/json',
+      },
+    });
 
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error('Login error:', error.response?.data || error.message);
+      throw error.response?.data || error.message;
+    } else {
+      console.error('Unexpected error:', error);
+      throw 'An unexpected erro occurred';
+    }
+  }
+};
 
 
 export { login };
--- Develop/client/src/utils/auth.ts
+++ Develop/client/src/utils/auth.ts
@@ -3,28 +3,46 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    return token ? jwtDecode<JwtPayload>(token) : null;
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token &&!this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const { exp } = jwtDecode<JwtPayload>(token);
+      if (!exp) {
+        return true;
+      }
+      return Date.now() >= exp * 1000;
+    } catch (err) {
+      return true;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    return localStorage.getItem('token') || '';
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('token', idToken);
     // TODO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('token');
     // TODO: redirect to the login page
+    window.location.assign('/login');
   }
 }
 
